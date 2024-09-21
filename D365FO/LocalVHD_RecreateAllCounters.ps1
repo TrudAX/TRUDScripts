d5ad43e424551c9e4d7690261e584af381c0bf78
Denis Trunin <trud81@gmail.com>
2024-09-21 08:01:12 +0000

    Add files via upload

---
+++ D365FO/LocalVHD_RecreateAllCounters.ps1
@@ -0,0 +1,28 @@
+﻿# Before running this copy C:\Monitoring\Instrumentation\ to C:\Monitoring\Instrumentation\Monitoring
+# and check APPROOT value, it should be C:\Monitoring\Instrumentation. PS: Get-ChildItem Env: | Sort-Object Name
+$directoryPath = "C:\Monitoring\Instrumentation"
+
+$manFiles = Get-ChildItem -Path $directoryPath -Filter "*.man"
+
+foreach ($file in $manFiles) {
+    $filePath = $file.FullName
+    
+    Write-Host "Processing file: $filePath"
+    
+    try {
+        Write-Host "Uninstalling manifest..."
+        wevtutil um $filePath
+        
+        Write-Host "Installing manifest..."
+        wevtutil im $filePath
+        
+        Write-Host "Successfully processed $filePath"
+    }
+    catch {
+        Write-Host "Error processing $filePath : $_" -ForegroundColor Red
+    }
+    
+    Write-Host "" # Empty line for better readability
+}
+
+Write-Host "All .man files have been processed."
